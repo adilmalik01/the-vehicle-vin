bbb1f71a99baec3b106e7014254082b8a7a85e6e
unknown <malikadil5679@gmail.com>
2024-08-17 20:58:33 +0500

    some changes on home page & navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,91 +12,91 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <nav className="bg-white border-b">
-        <div className="max-w-7xl m-auto px-9 sm:px-6 lg:px-6">
-          <div className="flex items-center justify-between px-12 h-[90px]">
-            <div className="flex justify-center">
-              <div className="flex-shrink-0">
-                <a href="/">
-                  <Image src="/logo.png" alt="logo" width={170} height={80} />
-                </a>
-              </div>
-            </div>
-
-            <div className="hidden lg:flex items-center space-x-4 text-black/80">
-              <a
-                href="/vin-check"
-                className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-medium border-transparent hover:border-black active:border-black px-3 py-2"
-              >
-                Vin check
+    <nav className="bg-white border-b">
+      <div className="max-w-7xl m-auto px-9 sm:px-6 lg:px-6">
+        <div className="flex items-center justify-between px-12 max-lg:h-[70px]  h-[90px]">
+          <div className="flex justify-center">
+            <div className="flex-shrink-0">
+              <a href="/">
+                <Image src="/logo.png" alt="logo" width={170} height={80} />
               </a>
-              <a
-                href="/dealer-program"
-                className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-medium border-transparent hover:border-black px-3 py-2"
-              >
-                Dealer Program
-              </a>
-              <a
-                href="/data-product"
-                className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-medium border-transparent hover:border-black px-3 py-2"
-              >
-                Data product
-              </a>
-              <a
-                href="/contact-us"
-                className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-medium border-transparent hover:border-black px-3 py-2"
-              >
-                Contact us
-              </a>
-            </div>
-
-            <div className="lg:hidden flex items-center">
-              {isClick ? (
-                <button onClick={toggleNavbar}>
-                  <Icon.XLg size={30} />
-                </button>
-              ) : (
-                <button onClick={toggleNavbar}>
-                  <Icon.List size={30} />
-                </button>
-              )}
             </div>
           </div>
-        </div>
 
-        <div
-          className={`lg:hidden transition-all duration-300 ease-out overflow-hidden ${isClick ? 'max-h-60 opacity-100' : 'max-h-0 opacity-0'}`}
-        >
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <div className="hidden lg:flex items-center  space-x-4 text-black/80">
             <a
               href="/vin-check"
-              className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+              className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-semibold border-transparent hover:border-black active:border-black px-3 py-2"
             >
               Vin check
             </a>
             <a
               href="/dealer-program"
-              className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+              className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-semibold border-transparent hover:border-black px-3 py-2"
             >
               Dealer Program
             </a>
             <a
               href="/data-product"
-              className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+              className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-semibold border-transparent hover:border-black px-3 py-2"
             >
               Data product
             </a>
             <a
               href="/contact-us"
-              className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+              className="border-b-[3px] transition-all duration-700 text-[16px] tracking-tight font-semibold border-transparent hover:border-black px-3 py-2"
             >
               Contact us
             </a>
           </div>
+
+          <div className="lg:hidden  flex items-center">
+            {isClick ? (
+              <button className=" bg-red-300" onClick={toggleNavbar}>
+                <Icon.XLg size={25} />
+              </button>
+            ) : (
+              <button onClick={toggleNavbar}>
+                <Icon.List size={25} />
+              </button>
+            )}
+          </div>
         </div>
-      </nav>
-    </>
+      </div>
+
+      <div
+        className={`lg:hidden transition-all duration-300 ease-out overflow-hidden ${
+          isClick ? "max-h-60 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="/vin-check"
+            className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+          >
+            Vin check
+          </a>
+          <a
+            href="/dealer-program"
+            className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+          >
+            Dealer Program
+          </a>
+          <a
+            href="/data-product"
+            className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+          >
+            Data product
+          </a>
+          <a
+            href="/contact-us"
+            className="border-b-[3px] block transition-all duration-300 tracking-tight font-semibold border-transparent hover:bg-[#33373d] hover:text-white px-3 py-2"
+          >
+            Contact us
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,40 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="">
       <Navbar />
-      <div className=" w-full h-[55vh] max-lg:min-h-[100vh]  flex max-lg:flex-col ">
-        <div className="bg-[#28386F] lg:w-[60%] max-lg:h-[100vh] max-lg:w-[100%]"></div>
-        <div className="bg-[#303030] lg:w-[35%] flex  justify-center items-center   max-lg:h-[60vh]       max-lg:w-[100%] ">
-          <Image src="/home-pic-1.png" alt="logo"  width={1900} height={180} />
+      <div className="bg-[#28386F]  lg:h-[75vh] w-full min-lg:h-[60vh]  lg:flex">
+        <div className="w-[60%] max-lg:h-[80vh] flex  justify-center items-center max-lg:w-[100%]">
+          <div className=" flex w-[85%] max-lg:h-full  max-lg:justify-center max-lg:text-center max-lg:items-center h-[80%] justify-start lg:items-start flex-col gap-8 ">
+            <h1 className="text-[4.5vw] max-md:text-[4.5vw] leading-tight text-white font-semibold">
+              EVERY VEHICLE <br /> HAS A STORY
+            </h1>
+            <p className="text-gray-200 text-[15px] font-medium">
+              Avoid buying a vehicle with costly hidden problems.
+            </p>
+            <ol className="text-white mt-4 font-medium text-[16px]">
+              <li> 1.More Than 100 Other Checks</li>
+              <li> 2.Title & Ownership History</li>
+              <li> 3.Previous Accidents</li>
+            </ol>
+          </div>
         </div>
-        <div className="bg-[#28386F] lg:w-[10%]  max-lg:h-[3vh]     max-lg:w-[100%]"></div>
+        <div className="bg-[#303030] w-[35%] relative max-lg:w-full max-lg:h-[40vh] flex  justify-center items-center  ">
+          <Image
+            className="   absolute top-[160px] "
+            src="/home-pic-1.png"
+            alt="logo"
+            width={600}
+            height={180}
+          />
+        </div>
+        <div className="w-[10%]  max-lg:w-full max-lg:h-[30vh]"> </div>
       </div>
+      
+      <section className="  z-10  relative w-full flex justify-center items-center h-[40vh] ">
+           <div className="w-[60%]  rounded-lg h-full bg-white shadow-lg"></div>
+      </section>
+      
     </main>
   );
 }
